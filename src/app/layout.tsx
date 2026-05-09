af203f3bc60e76a5f3299c124cac0ce25618b1b4
Tocki28 <138053880+Tocki28@users.noreply.github.com>
2026-05-09 16:41:10 +0300

    feat: add Cloudflare Web Analytics to app.gerendo.com

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
       className={`${fraunces.variable} ${inter.variable} antialiased`}
     >
       <body>{children}</body>
+      <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "74829cc5e584424cb58cbdbd373b5dd6"}' />
     </html>
   );
 }
